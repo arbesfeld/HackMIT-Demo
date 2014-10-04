103140b0691b4b635dd27d60f84a8f71c1e6b2a8
Matthew Arbesfeld <arbesfeld@gmail.com>
2014-10-04 02:58:19 -0400

    Add a camera which takes photos

--- HackMIT-Demo.js
+++ HackMIT-Demo.js
@@ -1,23 +1,22 @@
+var Photos = new Meteor.Collection("photos");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
+  Template.body.helpers({
+    photos: function () {
+      return Photos.find({}, {sort: {"createdAt": -1}});
     }
   });
 
-  Template.hello.events({
+  Template.body.events({
     'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
-    }
-  });
-}
+      var cameraOptions = {
+        width: 400,
+        height: 300
+      };
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+      MeteorCamera.getPicture(cameraOptions, function (error, data) {
+        Photos.insert({data: data});
+      });
+    }
   });
 }
